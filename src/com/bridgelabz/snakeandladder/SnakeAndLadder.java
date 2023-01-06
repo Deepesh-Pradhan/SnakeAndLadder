02ab4e28ba6501cba2d591b1949838a782e639a2
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-07 00:03:34 +0530

    Rolling the dice to get 1 to 6

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.snakeandladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
     static int P1 = 0;
@@ -8,6 +10,10 @@ public class SnakeAndLadder {
 
         System.out.println("   WELCOME TO SNAKE AND LADDER SIMULATOR");
         System.out.println("Welcome Player 1");
-        System.out.println("PLayer 1 is at position => " + P1);
+        Random random = new Random();
+        int roll = random.nextInt(6);
+        int out = roll + 1;
+        System.out.println("Player 1 rolled => "+out);
+        System.out.println("PLayer 1 is at position => " + P1 + out);
     }
 }
