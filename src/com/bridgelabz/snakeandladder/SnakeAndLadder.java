0343e657a468d8a229df551d3506c698d5c37ecb
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-06 23:51:21 +0530

    WELCOME TO SNAKE AND LADDER SIMULATOR

---
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeAndLadder {
+
+    public static void main(String[] args) {
+
+        System.out.println("WELCOME TO SNAKE AND LADDER SIMULATOR");
+
+    }
+}
