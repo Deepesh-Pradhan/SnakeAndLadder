52f1666f3788083eb255b67e129069c32fa7c541
deepeshpradhan <dpradhan0610@gmail.com>
2023-01-07 00:20:17 +0530

    Repeat till the Player

--- src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladder/SnakeAndLadder.java
@@ -9,27 +9,31 @@ public class SnakeAndLadder {
         System.out.println("   WELCOME TO SNAKE AND LADDER SIMULATOR");
         System.out.println("Welcome Player 1");
         int p1 = 0;
-        Random random = new Random();
-        int roll = random.nextInt(5);
-        int out = roll + 1;
-        int play = random.nextInt(3);
-        int move = 0;
-        if (play == 1) {
-            move = move - out;
-            p1 = move;
-            System.out.println("Player 1 rolled => " + out);
-            System.out.println("player 1 found a Snake and moved behind");
+        while (p1 <= 100) {
+            if (p1 < 0) {
+                p1 = 0;
+                continue;
+            }
+            Random random = new Random();
+            int roll = random.nextInt(6);
+            int out = roll + 1;
+            System.out.println(roll + "  " + out);
+            int play = random.nextInt(3);
+            if (play == 1) {
+                p1 = p1 - out;
+                System.out.println("Player 1 rolled => " + out);
+                System.out.println("player 1 found a Snake and moved behind");
+            } else if (play == 2) {
+                p1 = p1 + out;
+                System.out.println("Player 1 rolled => " + out);
+                System.out.println("Player 1 moved ahead using Ladder");
+            } else {
+                System.out.println("Player 1 rolled => " + out);
+                System.out.println("Player 1 cant move forward");
+            }
             System.out.println("PLayer 1 is at position => " + p1);
-        } else if (play == 2) {
-            move = move + out;
-            p1 = move;
-            System.out.println("Player 1 rolled => " + out);
-            System.out.println("Player 1 moved ahead using Ladder");
-            System.out.println("PLayer 1 is at position => " + p1);
-        } else {
-            System.out.println("Player 1 rolled => " + out);
-            System.out.println("Player 1 cant move forward");
-            System.out.println("PLayer 1 is at position => " + p1);
+            System.out.println("    ");
         }
+        System.out.println("Player 1 has completed the Game");
     }
 }
